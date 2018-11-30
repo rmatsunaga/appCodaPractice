afd40b3ec0fa550cd4604c42900238c661658eec
Rey Matsunaga <matsunagar97@gmail.com>
2018-11-29 18:45:48 -0800

    Stack view exercise

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Rey Matsunaga on 11/29/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
