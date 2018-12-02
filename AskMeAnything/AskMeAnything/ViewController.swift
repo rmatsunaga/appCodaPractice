d1bd41ae70e681edb9d250cbc79cabb3ebb3d544
Rey <matsunagar97@gmail.com>
2018-12-01 21:52:37 -0800

    stack and size class exercise

---
+++ AskMeAnything/AskMeAnything/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AskMeAnything
+//
+//  Created by Rey Matsunaga on 12/1/18.
+//  Copyright © 2018 Rey Matsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
