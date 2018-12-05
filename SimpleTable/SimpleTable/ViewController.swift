99ef647619eab6e29d2ae031f781843133ccb92d
Rey <matsunagar97@gmail.com>
2018-12-04 19:07:10 -0800

    Table View Project

---
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Rey Matsunaga on 12/4/18.
+//  Copyright © 2018 Rey Matsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
